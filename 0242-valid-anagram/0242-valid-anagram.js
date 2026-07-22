0bb43bf88928d92426ba7ca1078c02d654df4b25
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-07-22 11:55:14 +0530

    Time: 30 ms (15.63%), Space: 58.1 MB (41.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length != t.length){
+        return false;
+    }
+
+    let arr1 = s.split("").sort().join("");
+    let arr2 = t.split("").sort().join("");
+
+    return arr1 == arr2;
+};
