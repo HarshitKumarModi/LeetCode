22e7282830a362552989f883c1779f61f9b58a7a
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-08-02 11:43:15 +0530

    Time: 3 ms (86.07%), Space: 43.2 MB (73.63%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,33 @@
 import java.util.Stack;
 
 public class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } 
-            else {
-                if (stack.isEmpty()) return false;
-
-                char top = stack.pop();
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
+        for(int i = 0; i<n; i++){
+            char ch  = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            } else {
+                if(st.size() == 0) return false;
+                if((ch == ')' && st.peek() != '(') ||
+                  (ch == ']' && st.peek() != '[') ||
+                  (ch == '}' && st.peek() != '{')) {
                     return false;
                 }
+                st.pop();
+
             }
         }
-        return stack.isEmpty();
+        if(st.size() > 0) return false;
+        else return true;
+
     }
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        
-        System.out.println(solution.isValid("()"));       
-        System.out.println(solution.isValid("()[]{}"));   
-        System.out.println(solution.isValid("(]"));       
-        System.out.println(solution.isValid("([])"));     
-    }
+
+     public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            System.out.println(isValid(s));
+        } 
 }
