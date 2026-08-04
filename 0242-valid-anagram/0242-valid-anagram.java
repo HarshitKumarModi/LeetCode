84ea275b0c3ec6ad2cea3c2e38a32f99a6672488
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-08-04 22:07:30 +0530

    Time: 18 ms (13.28%), Space: 46.8 MB (5.13%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(mp.containsKey(ch)){
+                mp.put(ch, mp.get(ch) + 1);
+            } else {
+                mp.put(ch, 1);
+            }
+        }
+        for(int i = 0; i<t.length(); i++){
+            char ch = t.charAt(i);
+
+            if(!mp.containsKey(ch)){
+                return false;
+            }
+            mp.put(ch, mp.get(ch) - 1);
+
+            if(mp.get(ch) == 0){
+                mp.remove(ch);
+            }
+
+            
+        }
+        return mp.isEmpty();
+    }
+}
