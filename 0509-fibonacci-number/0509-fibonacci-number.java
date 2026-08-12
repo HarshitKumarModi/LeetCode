1db946c042118ac125e727b0a47b70eb5b754c65
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-08-12 22:39:10 +0530

    Time: 9 ms (43.19%), Space: 42.2 MB (33.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,25 +1,25 @@
 // using recursion
-// class Solution {
-//     public int fib(int n) {
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+           return n;
+        }
+       return fib(n-1) + fib(n-2);
+    }
+}
+// using simple for loops
+// class Solution{
+//     public int fib(int n){
 //         if(n<=1){
 //             return n;
 //         }
-//         return fib(n-1) + fib(n-2);
+//         int a = 0;
+//         int b = 1;
+//         for(int i = 2; i<=n; i++){
+//             int c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//         return b;
 //     }
 // }
-// using simple for loops
-class Solution{
-    public int fib(int n){
-        if(n<=1){
-            return n;
-        }
-        int a = 0;
-        int b = 1;
-        for(int i = 2; i<=n; i++){
-            int c = a + b;
-            a = b;
-            b = c;
-        }
-        return b;
-    }
-}
