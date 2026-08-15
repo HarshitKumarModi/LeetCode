03f911214990d76f901a2274d11991be0084fd33
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-08-15 21:32:49 +0530

    Time: 3 ms (86.11%), Space: 43.5 MB (15.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,30 +4,24 @@ public class Solution {
     public static boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         int n = s.length();
-
         for(int i = 0; i<n; i++){
-            char ch  = s.charAt(i);
-            if(ch == '(' || ch == '[' || ch == '{'){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
                 st.push(ch);
             } else {
                 if(st.size() == 0) return false;
-                if((ch == ')' && st.peek() != '(') ||
-                  (ch == ']' && st.peek() != '[') ||
-                  (ch == '}' && st.peek() != '{')) {
+                if((ch == ')' && st.peek()!= '(') ||
+                   (ch == '}' && st.peek()!= '{') ||
+                   (ch == ']' && st.peek()!= '[')){
                     return false;
+                } else {
+                    st.pop();
                 }
-                st.pop();
-
+                  
             }
         }
-        if(st.size() > 0) return false;
+        if(st.size()> 0) return false;
         else return true;
 
-    }
-
-     public static void main(String[] args){
-            Scanner sc = new Scanner(System.in);
-            String s = sc.nextLine();
-            System.out.println(isValid(s));
-        } 
+    } 
 }
