f7bb4f45454002c44149cb0af35beeaf63669bc8
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-08-27 00:13:20 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,27 +1,18 @@
 import java.util.*;
 class Solution {
-    public int ways(int sr, int sc, int er, int ec, int[][] dp){
-        if(sr > er || sc > ec){
-            return 0;
-        }
-        if(sr == er && sc == ec){
-            return 1;
-        }
-        if(dp[sr][sc]!= -1){
-            return dp[sr][sc];
-        }
-        int rightways = ways(sr,sc+1,er,ec,dp);
-        int downways = ways(sr+1,sc,er,ec,dp);
-
-        dp[sr][sc] = rightways + downways;
-        return dp[sr][sc];
-
-    }
     public int uniquePaths(int m, int n){
         int[][] dp = new int[m][n];
         for(int i = 0; i<m; i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][0] = 1;
         }
-        return ways(0,0,m-1,n-1,dp);
+        for(int j = 0; j<n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
