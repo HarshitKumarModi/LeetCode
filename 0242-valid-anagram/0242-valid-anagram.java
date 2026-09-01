e5609e70cb76abe13fd252205fcc0e7023b698d9
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-02 00:56:46 +0530

    Time: 22 ms (6.43%), Space: 47 MB (9.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,28 @@
 import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
+        if(s.length() != t.length()) return false;
 
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
-
             if(map.containsKey(ch)){
                 map.put(ch, map.get(ch) + 1);
             } else {
-                map.put(ch, 1);
+                map.put(ch , 1);
             }
         }
-        for(int i = 0; i<t.length(); i++){
-            char ch = t.charAt(i);
 
-            if(!map.containsKey(ch)){
+        for(int i = 0; i<t.length(); i++){
+            char c = t.charAt(i);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c) - 1);
+            } else{
                 return false;
-            } else {
-                map.put(ch, map.get(ch) - 1);
             }
-            
-            if(map.get(ch) == 0){
-                map.remove(ch);
+            if(map.get(c) == 0){
+                map.remove(c);
             }
         }
         return map.isEmpty();
