39a04661314ca09a21b951021518bca9ad920786
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-09 23:11:43 +0530

    Time: 4 ms (60.33%), Space: 50.3 MB (38.7%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -12,7 +12,7 @@ class Solution {
         }
         for(int i = 1; i<m; i++){
             for(int j = 1; j<n; j++){
-                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
         return dp[m-1][n-1];
