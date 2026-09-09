28ae6173eb8fdcee0e3299a1b7d84047634de434
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-09 22:32:10 +0530

    Time: 0 ms (100%), Space: 42.8 MB (60.24%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -4,19 +4,19 @@ class Solution {
         if(len == 1) return nums[start];
         int[] dp = new int[len];
         dp[0] = nums[start];
-        dp[1] = Math.max(nums[start],nums[start+1]);
+        dp[1] = Math.max(nums[start], nums[start+1]);
         for(int i = 2; i<len; i++){
-            dp[i] = Math.max(nums[start+i]+dp[i-2],dp[i-1]);
+            dp[i] = Math.max(dp[i-1], (nums[start+i] + dp[i-2]));
         }
-        return dp[len-1];
+        return dp[len - 1];
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
 
-        int case1 = solve(nums,0,n-2);
-        int case2 = solve(nums,1,n-1);
+        int a = solve(nums, 0, n-2);
+        int b = solve(nums, 1, n-1);
 
-        return Math.max(case1,case2);
+        return Math.max(a, b);
     }
 }
