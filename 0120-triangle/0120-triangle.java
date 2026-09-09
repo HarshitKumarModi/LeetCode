8d5f41a167655e870b54f2cc89fa7d265d01b258
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-09 23:42:32 +0530

    Time: 3 ms (78.9%), Space: 45.6 MB (40.36%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,12 +1,9 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        // bottom up appraoch
         int n = triangle.size();
         int[][] dp = new int[n][n];
-        // copy the last row
-        for(int j = 0; j<n; j++){
-            dp[n-1][j] = triangle.get(n-1).get(j);
-
+        for(int i = 0; i<n; i++){
+            dp[n-1][i] = triangle.get(n-1).get(i);
         }
 
         for(int i = n-2; i>=0; i--){
@@ -14,6 +11,7 @@ class Solution {
                 dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
+
         return dp[0][0];
     }
 }
