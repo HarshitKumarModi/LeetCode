417ff154b53acd629a4463b0994be7daf48e3c2f
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-17 11:32:54 +0530

    Time: 16 ms (74.48%), Space: 44.3 MB (74.86%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        int m = cuts.length;
+
+        int[] points = new int[m+2];
+
+        points[0] = 0;
+        points[m+1] = n;
+
+        for(int i = 0; i<m; i++){
+            points[i+1] = cuts[i];
+        }
+
+        Arrays.sort(points);
+
+        int[][] dp = new int[m+2][m+2];
+
+        for(int i = m; i>=1; i--){
+            for(int j = i; j<=m; j++){
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for(int k = i; k<=j; k++){
+                    int cost = dp[i][k-1] + dp[k+1][j] + points[j+1] - points[i-1];
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[1][m];
+    }
+}
