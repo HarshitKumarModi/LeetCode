73521831c8a495fda3f9efc952d1b05be01d0d53
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-20 15:52:00 +0530

    Time: 2 ms (99.96%), Space: 43 MB (78.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -5,25 +5,25 @@ class Solution {
         for(int num : nums){
             sum += num;
         }
-
-        int subsetSum = (sum+target) / 2;
+        int subsetTarget = (sum+target) / 2;
 
         if(Math.abs(target) > sum){
             return 0;
         }
-        if((sum+target) % 2 != 0){
+        if((target+sum) % 2 != 0){
             return 0;
         }
 
-        int[] dp = new int[subsetSum + 1];
+        int[] dp = new int[subsetTarget+1];
 
         dp[0] = 1;
 
-        for(int i = 0; i<n; i++){
-            for(int j = subsetSum; j>=nums[i]; j--){
-                dp[j] = dp[j] + dp[j-nums[i]];
+        for(int num : nums){
+            for(int j = subsetTarget; j>=num; j--){
+                dp[j] = dp[j]+dp[j-num];
             }
         }
-        return dp[subsetSum];
+
+        return dp[subsetTarget];
     }
 }
