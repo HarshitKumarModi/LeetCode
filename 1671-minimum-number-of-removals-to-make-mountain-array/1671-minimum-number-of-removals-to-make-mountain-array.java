c7ee014a770644fc63421d17926977cf7ada11b8
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-20 23:00:30 +0530

    Time: 38 ms (65.92%), Space: 46.3 MB (81.51%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -7,17 +7,15 @@ class Solution {
 
         for(int i = 0; i<n; i++){
             left[i] = 1;
-
             for(int j = 0; j<i; j++){
                 if(nums[j] < nums[i]){
-                    left[i] = Math.max(left[i], left[j]+1);
+                    left[i] = Math.max(left[i], 1+left[j]);
                 }
             }
         }
 
         for(int i = n-1; i>=0; i--){
             right[i] = 1;
-
             for(int j = n-1; j>i; j--){
                 if(nums[j] < nums[i]){
                     right[i] = Math.max(right[i], 1+right[j]);
@@ -30,11 +28,9 @@ class Solution {
         for(int i = 0; i<n; i++){
             if(left[i] > 1 && right[i] > 1){
                 int mountainLength = left[i] + right[i] - 1;
-
                 maxMountain = Math.max(maxMountain, mountainLength);
             }
         }
-
         return n-maxMountain;
     }
 }
