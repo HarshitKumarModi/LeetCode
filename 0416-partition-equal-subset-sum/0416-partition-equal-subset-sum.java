99537cb5105ee933a5771a2ba59d20255899660d
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-20 15:27:08 +0530

    Time: 64 ms (39.74%), Space: 47.6 MB (56.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,16 +1,21 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
+
         int sum = 0;
+
         for(int num : nums){
             sum += num;
         }
+
+        int target = sum / 2;
+
         if(sum % 2 != 0){
             return false;
         }
-        int target = sum / 2;
 
         boolean[][] dp = new boolean[n+1][target+1];
+
         for(int i = 0; i<=n; i++){
             dp[i][0] = true;
         }
@@ -20,7 +25,7 @@ class Solution {
                 dp[i][j] = dp[i-1][j];
 
                 if(nums[i-1] <= j){
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - nums[i-1]];
                 }
             }
         }
