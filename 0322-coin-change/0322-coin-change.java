1181563b79e0855f3b7a8a2e53dbb1820b65e003
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-20 20:38:18 +0530

    Time: 13 ms (93.89%), Space: 46.1 MB (92.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,6 +2,7 @@ public class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[] dp = new int[amount+1];
+
         Arrays.fill(dp, amount+1);
 
         dp[0] = 0;
@@ -9,11 +10,12 @@ public class Solution {
         for(int i = 0; i<n; i++){
             for(int j = 1; j<=amount; j++){
                 if(coins[i] <= j){
-                    dp[j] = Math.min(dp[j], 1+dp[j-coins[i]]);
+                    dp[j] = Math.min(dp[j], 1+dp[j - coins[i]]);
                 }
             }
         }
-        if(dp[amount] == amount + 1){
+
+        if(dp[amount] == amount+1){
             return -1;
         }
 
