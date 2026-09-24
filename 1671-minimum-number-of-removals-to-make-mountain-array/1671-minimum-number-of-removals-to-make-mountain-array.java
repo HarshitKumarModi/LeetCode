b586cfaa8abac4fa9d4f22d1aaa9879a282ae5b2
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-24 23:17:39 +0530

    Time: 40 ms (45.58%), Space: 46.2 MB (81.77%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,19 +1,19 @@
 class Solution {
     public int minimumMountainRemovals(int[] nums) {
         int n = nums.length;
-
+        
         int[] left = new int[n];
         int[] right = new int[n];
-
+        
         for(int i = 0; i<n; i++){
             left[i] = 1;
             for(int j = 0; j<i; j++){
                 if(nums[j] < nums[i]){
-                    left[i] = Math.max(left[i], 1 + left[j]);
+                    left[i] = Math.max(left[i], 1+left[j]);
                 }
             }
         }
-
+        
         for(int i = n-1; i>=0; i--){
             right[i] = 1;
             for(int j = n-1; j>i; j--){
@@ -22,16 +22,17 @@ class Solution {
                 }
             }
         }
-
+        
         int maxMountain = 0;
-
+        
+        
         for(int i = 0; i<n; i++){
-            if(left[i] > 1  && right[i] > 1){
-                int mountainLength = left[i]+right[i]-1;
+            if(left[i] > 1 && right[i] > 1){
+                int mountainLength = left[i] + right[i] - 1;
                 maxMountain = Math.max(maxMountain, mountainLength);
             }
         }
-
-        return n-maxMountain;
+        
+        return n-maxMountain;    
     }
 }
