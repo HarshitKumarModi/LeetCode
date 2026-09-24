b7b4c5292d58bad3da96313ffdf4396357b55603
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-24 22:29:21 +0530

    Time: 56 ms (41.36%), Space: 63.4 MB (83.39%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -7,7 +7,7 @@ class Solution {
 
         int[][] dp = new int[m+1][n+1];
 
-        for(int i = m-1; i>=0; i--){
+        for(int i = n-1; i>=0; i--){
             for(int j = n-1; j>=0; j--){
                 if(s.charAt(i) == t.charAt(j)){
                     dp[i][j] = 1 + dp[i+1][j+1];
@@ -16,7 +16,6 @@ class Solution {
                 }
             }
         }
-
-        return dp[0][0];  
+        return dp[0][0];
     }
 }
