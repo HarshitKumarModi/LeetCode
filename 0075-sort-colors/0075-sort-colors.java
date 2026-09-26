0077ca783740c9fe3500eaab7aa27f322f251e75
HarshitKumarModi <harshit.23bce10764@vitbhopal.ac.in>
2026-09-26 18:55:23 +0530

    Time: 0 ms (100%), Space: 43.2 MB (88.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -6,18 +6,18 @@ class Solution{
     }
     public void sortColors(int[] nums){
         int n = nums.length;
-        int mid = 0;
         int low = 0;
+        int mid = 0;
         int high = n-1;
-        while(mid<=high){
+        while(mid <= high){
             if(nums[mid] == 0){
-                swap(nums,low,mid);
+                swap(nums, low, mid);
                 low++;
                 mid++;
-            }else if(nums[mid] == 1){
+            } else if (nums[mid] == 1){
                 mid++;
-            }else{
-                swap(nums,mid,high);
+            } else {
+                swap(nums, mid, high);
                 high--;
             }
         }
